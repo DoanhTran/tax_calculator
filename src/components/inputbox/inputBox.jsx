fb5e6f37c3aeae40808ac230ee3dcc5efae0d612
Doanh <ddt45@cornell.edu>
2020-07-14 18:25:56 -0700

    created input box that shakes when input exceeds 5 characters

--- src/components/inputbox/inputBox.jsx
+++ src/components/inputbox/inputBox.jsx
@@ -5,10 +5,15 @@ export default function InputBox (){
 
     const [zipcode, setZipcode] = useState();
     const [save, setSave] = useState(false);
+    const [animation, setAnimation] = useState();
 
     const handleOnChange = (event) => {
+     
+        console.log("animation", animation);
+        
         const input = event.currentTarget.value;
-        setZipcode(onlyNum(input));
+        const returnInput = maxFive(input);
+        setZipcode(onlyNum(returnInput));
 
     }
 
@@ -20,19 +25,30 @@ export default function InputBox (){
 
 
 
-    /* Extract only numbers out of input box and returns a string of text 
-    containing at most 6 numbers
+    /* Extract only numbers out of input
     Parameter: text is a string
     Returns a string of only numbers or empty string*/
     function onlyNum(text) {
+
         let lastChar = text.slice(-1);
         let ascii = lastChar.charCodeAt(0);
-
         if (48 <= ascii && ascii <= 57) {
         return text;
         } else {
         return text.slice(0, -1);
         }
+    }
+
+    function maxFive(text){
+
+        if (text.length > 5){
+            setAnimation('shake');
+            setTimeout(function() {
+                setAnimation();
+            }, 200);
+            return text.slice(0,5);
+        }
+        return text;
     }
       
     return(
@@ -42,8 +58,9 @@ export default function InputBox (){
                 Zipcode &#160;
             </label>
             <input
+                className ={animation}
                 type = "text"
-                maxLength = "5"
+                maxLength = "6"
                 value = {zipcode}
                 size = '10'
                 onChange = {handleOnChange}
@@ -54,4 +71,4 @@ export default function InputBox (){
 
         </form>
     )
-}
+ }
