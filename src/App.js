a16a2ce122f5baf8c358c5f1ac402a612a0b8b73
Jp889 <jp889@cornell.edu>
2020-07-17 17:52:20 +0700

    connect

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import InputBox from './components/inputbox/inputBox';
+
+function App() {
+  return (
+    <div>
+      <InputBox/>
+    </div>
+  );
+}
+
+export default App;
