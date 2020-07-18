6ecb427fde21048e24e99921477b1c7797661d57
Doanh <ddt45@cornell.edu>
2020-07-18 02:10:27 -0700

    wrote a dfs but nothing is working cries

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import InputBox from './components/inputbox/inputBox';
+import Test from './components/inputbox/Test';
+import Test2 from './components/Test2/Test2';
 
 function App() {
   return (
     <div>
+      <Test2/>
+      <Test/>
       <InputBox/>
     </div>
   );
---
+++ src/components/Test2/Test2.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect, useRef} from "react";
+//import Stack from 'Stack';
+/*global chrome*/
+
+export default function Test2 (){
+
+    const title = useRef(null)
+    const tab_title = useRef('')
+    var list = []
+
+
+    function result(array){
+        console.log(list.toString());
+    }
+    
+    // }
+    /*result is body element node i think ```o_o```*/
+    function findprice(node){
+        //node.setAttribute('visited', true);
+        //console.log("node's visited", node.getAttribute('visited'));
+        if (node.nodeType === 3 && node.nodeValue.includes('$')) {
+            list.append(node);
+            title.current.innerHTML = title + node.nodeValue;
+            console.log(list.length);
+        }
+        console.log('1');
+        if (node.hasChildNodes()){
+            
+            let children = node.childNodes;  
+            for (let i = 0; i < children.length; i++){
+                
+                findprice(children[i]);
+
+            }      
+        }
+        
+        
+        // if (node.childNodes.length === 0){
+        //     node.childNodes.forEach(element => {
+        //         findprice(element);    
+        //     });  
+        // }
+    }
+
+
+    const code = `function getDOM (){
+
+        function findprice(node){
+        
+            if (node.nodeType === 3 && node.nodeValue.includes('$')) {
+                list.append(node);
+                title.current.innerHTML = title + node.nodeValue;
+                console.log(list.length);
+            }
+            if (node.hasChildNodes()){
+                let children = node.childNodes;  
+                for (let i = 0; i < children.length; i++){
+                    findprice(children[i]);
+    
+                }      
+            }
+
+        findprice(document);
+    }`
+ 
+    chrome.tabs.query({active: true}, function(tabs) {
+        var tab = tabs[0];
+        list = [];
+        //tab_title.current = tab.title;
+
+        //hmm getting the body node
+        chrome.tabs.executeScript(tab.id, {
+            code
+        }, result);
+    });
+    
+
+    
+    // var tab_title = '';
+    // function display_h1 (results){
+    //     let h1=results;
+    //     document.querySelector("#id1").innerHTML = "<p>tab title: " + tab_title + "</p><p>dom h1: " + h1 + "</p>";
+    // }
+    // chrome.tabs.query({active: true}, function(tabs) {
+    // var tab = tabs[0];
+    // tab_title = tab.title;
+    // chrome.tabs.executeScript(tab.id, {
+    //     code: 'document.querySelector("h1").textContent'
+    // }, display_h1);
+    // });
+
+    return(
+    <h1 ref = {title} >hello</h1>
+    )
+}
