552e4ca9891e5b70989c16feddcc3800fa395d04
Doanh <ddt45@cornell.edu>
2020-08-12 21:20:20 -0700

    changed gitignore

---
+++ extension/build/precache-manifest.955c10d8840ce27c827a9c0db9b4eacb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d1702af7387b39f79cc1a67705e88d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c826a8a113f05f7fdc3b",
+    "url": "/static/css/app.css"
+  },
+  {
+    "revision": "8576f0d94df9590bd61e",
+    "url": "/static/css/content.css"
+  },
+  {
+    "revision": "cd7f4955c72b8423191c",
+    "url": "/static/js/0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c826a8a113f05f7fdc3b",
+    "url": "/static/js/app.js"
+  },
+  {
+    "revision": "8576f0d94df9590bd61e",
+    "url": "/static/js/content.js"
+  }
+]);
